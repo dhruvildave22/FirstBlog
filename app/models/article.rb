20ed57ac1ddc50106659ddb6069c9da38cda45fe
dhruvildave <dhruvildave22@gmail.com>
2019-04-30 11:54:17 +0530

    add association between user and article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+	belongs_to :user
+	validates :title, presence: true, length: { minimum: 3, maximum: 25 }
+	validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+	validate :user_id, presence: true
 end
